d673de5cd0f770935f5feaf59283f3444b723755
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-04-16 14:52:59 +0500

    first react app created

---
+++ 01 installation and setup/my-first-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold text-blue-500">
+          Hello, Tailwind Works
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
